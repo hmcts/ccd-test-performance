7b2030102881a43c06551b23663f6287428f8736
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2018-05-22 15:20:45 +0100

    added passing jar path to launch script

--- launch.sh
+++ launch.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
-COMPILATION_CLASSPATH=`find -L ./target -maxdepth 1 -name "*fat-tests.jar" -type f -exec printf :{} ';'`
+
+if [ -z "$1" ];
+then
+    echo "Simulation launch script"
+    echo
+    echo "Required Parameters : "
+    echo
+    echo " <jar directory path> : directory containing the performance tests jar. Required"
+    echo
+   exit 0;
+ fi
+
+COMPILATION_CLASSPATH="$(find -L "$1" -maxdepth 1 -name "*fat-tests.jar" -type f -exec printf :{} ';')"
 JAVA_OPTS="-server -XX:+UseThreadPriorities -XX:ThreadPriorityPolicy=42 -Xms512M -Xmx2048M -XX:+HeapDumpOnOutOfMemoryError -XX:+AggressiveOpts -XX:+OptimizeStringConcat -XX:+UseFastAccessorMethods -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false ${JAVA_OPTS}"
-java $JAVA_OPTS -cp $COMPILATION_CLASSPATH io.gatling.app.Gatling
+ENVIRONMENT=$2 java $JAVA_OPTS -cp $COMPILATION_CLASSPATH io.gatling.app.Gatling
