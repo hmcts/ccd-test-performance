e7159186e4056459fa244f80e6d8b64dc6262bce
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2018-05-29 16:00:14 +0100

    fixed launch script

--- launch.sh
+++ launch.sh
@@ -13,4 +13,4 @@ then
 
 COMPILATION_CLASSPATH="$(find -L "$1" -maxdepth 1 -name "*fat-tests.jar" -type f -exec printf :{} ';')"
 JAVA_OPTS="-server -XX:+UseThreadPriorities -XX:ThreadPriorityPolicy=42 -Xms512M -Xmx2048M -XX:+HeapDumpOnOutOfMemoryError -XX:+AggressiveOpts -XX:+OptimizeStringConcat -XX:+UseFastAccessorMethods -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false ${JAVA_OPTS}"
-ENVIRONMENT=$2 java $JAVA_OPTS -cp $COMPILATION_CLASSPATH io.gatling.app.Gatling
+java $JAVA_OPTS -cp $COMPILATION_CLASSPATH io.gatling.app.Gatling
