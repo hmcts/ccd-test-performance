baaa3259f51bb89808258ce50306c0c5cab7036e
kjain <Kapil.Jain@HMCTS.NET>
2019-03-22 11:58:49 +0000

    Adding Probate Data Injection

---
+++ src/test/scala/uk/gov/hmcts/ccd/data/CreateProbateGrantCaseData.scala
@@ -0,0 +1,143 @@
+package uk.gov.hmcts.ccd.data
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import uk.gov.hmcts.ccd.util.{CcdTokenGenerator, PerformanceTestsConfig}
+
+import scala.concurrent.duration._
+import scala.util.Random
+
+
+object CreateProbateGrantCaseData extends PerformanceTestsConfig {
+
+
+  private val rng: Random = new Random()
+  private def applicationFeePaperForm(): Int = rng.nextInt(9999999)
+  private def feeForCopiesPaperForm(): Int = rng.nextInt(99999)
+  private def totalFeePaperForm(): Int = rng.nextInt(99999999)
+  private def paymentReferenceNumberPaperform(): Int = rng.nextInt(99999999)
+  private def primaryApplicantForenamesTXT(): String = rng.alphanumeric.take(10).mkString
+  private def primaryApplicantForenames(): Int = rng.nextInt(99999999)
+  private def primaryApplicantSurnameTXT(): String = rng.alphanumeric.take(10).mkString
+  private def primaryApplicantSurname(): Int = rng.nextInt(99999999)
+
+  private def primaryApplicantPhoneNumber(): Int = rng.nextInt(999999)
+  private def primaryApplicantSecondPhoneNumber(): Int = rng.nextInt(999999)
+  private def primaryApplicantEmailAddress(): Int = rng.nextInt(999999)
+  private def primaryApplicantAddress(): Int = rng.nextInt(9999)
+  private def deceasedForenames(): Int = rng.nextInt(999999)
+  private def deceasedSurname(): Int = rng.nextInt(999999)
+  private def deceasedAddress(): Int = rng.nextInt(9999)
+  private def ukEstateValue(): Int = rng.nextInt(99999999)
+  private def ihtGrossValue(): Int = rng.nextInt(99999999)
+
+
+  private def caseSummaryText(): String = rng.alphanumeric.take(20).mkString
+  private def caseDescriptionText(): String = rng.alphanumeric.take(30).mkString
+
+
+  val randcaseType = new Random(System.currentTimeMillis())
+  val caseEventTypeValue = Array("applyForGrant")
+  val caseTypeValue_random_index = randcaseType.nextInt(caseEventTypeValue.length)
+  val EventId = caseEventTypeValue(caseTypeValue_random_index)
+  def PickCaseType(): String = EventId
+ //def PickCaseType(): String = caseEventTypeValue(randcaseType.nextInt(caseEventTypeValue.length))
+
+  println("caseTypeText Value   " + EventId)
+  println("caseTypeText Value   " + PickCaseType())
+
+  var CreateCaseUrl = caseDataUrl(config.getString("createCaseProbateGrantApplicationUrl"))
+
+  println("create case url: " + CreateCaseUrl)
+
+  val caseTypeValue = Array("GrantOfRepresentation")
+
+  val jurisdictionsValue = Array("PROBATE")
+
+  val rand = new Random(System.currentTimeMillis())
+  val caseType_random_index = rand.nextInt(caseTypeValue.length)
+
+  val jurisdictions_random_index = rand.nextInt(jurisdictionsValue.length)
+
+  if(!CreateCaseUrl.contains(":casetype_reference"))
+    {
+      CreateCaseUrl
+    }
+  else{
+    CreateCaseUrl = CreateCaseUrl.replace(":casetype_reference", caseTypeValue(caseType_random_index))
+  }
+
+  println("create case url: " + CreateCaseUrl)
+
+  if(!CreateCaseUrl.contains(":jurisdictions_reference"))
+  {
+    CreateCaseUrl
+  }
+  else{
+    CreateCaseUrl = CreateCaseUrl.replace(":jurisdictions_reference", jurisdictionsValue(jurisdictions_random_index))
+  }
+
+  println("create case url: " + CreateCaseUrl)
+  val CreateCaseTokenUrl = s"${CreateCaseUrl.replaceAll("cases", "")}event-triggers/$EventId/token"
+
+
+  println("create case url: " + CreateCaseUrl)
+  println("create case token url: " + CreateCaseTokenUrl)
+
+  val CreateProbateGrantCaseDataEventBodyMain =StringBody("""{"data": {"registryLocation": "London","applicationType": "Personal","applicationSubmittedDate": "2018-12-12","caseType": "gop","extraCopiesOfGrant": "1","outsideUKGrantCopies": "1","applicationFeePaperForm": "${ApplicationFeePaperForm}","feeForCopiesPaperForm": "${FeeForCopiesPaperForm}","totalFeePaperForm": "${TotalFeePaperForm}","paperPaymentMethod": "debitOrCredit","paymentReferenceNumberPaperform": "PAYREF:${PaymentReferenceNumberPaperform}","primaryApplicantForenames": "PA application First Name ${PrimaryApplicantForenamesTXT} ${PrimaryApplicantForenames}","primaryApplicantSurname": "PA application Last Name ${PrimaryApplicantSurnameTXT} ${PrimaryApplicantSurname}","primaryApplicantPhoneNumber": "02920${PrimaryApplicantPhoneNumber}","primaryApplicantSecondPhoneNumber": "02020${PrimaryApplicantSecondPhoneNumber}","primaryApplicantEmailAddress": "PAApplicant${PrimaryApplicantEmailAddress}@confirmation.com","primaryApplicantAddress": {"AddressLine1": "${PrimaryApplicantAddress} Villa Road","AddressLine2": "","AddressLine3": "Stanway","PostTown": "Colchester","County": "","PostCode": "CO3 0RN","Country": "United Kingdom"},"primaryApplicantRelationshipToDeceased": "partner","primaryApplicantHasAlias": "No","primaryApplicantIsApplying": "Yes","otherExecutorExists": "No","notifiedApplicants": "No","adopted": "No","boDeceasedTitle": "Mr","deceasedForenames": "Deceased FirstName ${DeceasedForenames}","deceasedSurname": "Deceased Last ${DeceasedSurname}","boDeceasedHonours": "OBE","deceasedAddress": {"AddressLine1": "${DeceasedAddress} Landmark Place","AddressLine2": "Churchill Way","AddressLine3": "","PostTown": "Cardiff","County": "","PostCode": "CF10 2HT","Country": "United Kingdom"},"deceasedDateOfBirth": "1945-03-21","deceasedDateOfDeath": "2016-06-30","deceasedAnyOtherNames": "No","deceasedMartialStatus": "marriedCivilPartnership","foreignAsset": "No","willExists": "No","deceasedEnterMarriageOrCP": "No","dateOfDivorcedCPJudicially": "2018-12-12","courtOfDecree": "London","willGiftUnderEighteen": "Yes","spouseOrPartner": "Yes","applyingAsAnAttorney": "No","deceasedDomicileInEngWales": "No","domicilityCountry": "UK","ukEstate": [{"id": null,"value": {"item": "House","value": "${UkEstateValue}"}}],"domicilityIHTCert": "No","ihtFormCompletedOnline": "No","ihtFormId": "IHT205","ihtGrossValue": "${IhtGrossValue}","ihtNetValue": "${IhtGrossValue}"},"event": {"id": "applyForGrant","summary": "Performance Testing Event summary ${CaseSummaryText}","description": "Performance Testing Event description ${CaseDescriptionText}"},"event_token": """"+ "${eventToken}" + """","ignore_warning": false,"draft_id": null}""")
+
+
+  val token = CcdTokenGenerator.generateGatewayS2SToken()
+  val userToken = CcdTokenGenerator.generateWebUserToken(CreateCaseUrl)
+
+
+  val   CreateProbateGrantCaseDataSCN = scenario("Create Probate Grant Application Case Data").during(TotalRunDuration minutes) {
+    exec(
+      _.setAll(
+        ("ApplicationFeePaperForm", applicationFeePaperForm()),
+        ("FeeForCopiesPaperForm", feeForCopiesPaperForm()),
+        ("TotalFeePaperForm", totalFeePaperForm()),
+        ("PaymentReferenceNumberPaperform",paymentReferenceNumberPaperform()),
+        ("PrimaryApplicantForenamesTXT",primaryApplicantForenamesTXT()),
+        ("PrimaryApplicantForenames",primaryApplicantForenames()),
+        ("PrimaryApplicantSurnameTXT",primaryApplicantSurnameTXT()),
+        ("PrimaryApplicantSurname",primaryApplicantSurname()),
+        ("CaseSummaryText",caseSummaryText()),
+        ("CaseDescriptionText",caseDescriptionText()),
+        ("PrimaryApplicantPhoneNumber", primaryApplicantPhoneNumber()),
+        ("PrimaryApplicantSecondPhoneNumber",primaryApplicantSecondPhoneNumber()),
+        ("PrimaryApplicantEmailAddress",primaryApplicantEmailAddress()),
+        ("PrimaryApplicantAddress",primaryApplicantAddress()),
+        ("PickCaseEventType",PickCaseType()),
+        ("DeceasedForenames", deceasedForenames()),
+        ("DeceasedSurname", deceasedSurname()),
+        ("DeceasedAddress", deceasedAddress()),
+        ("UkEstateValue", ukEstateValue()),
+        ("IhtGrossValue", ihtGrossValue())
+      )
+    ).exec(
+      //http("get create case event token")
+      http("TX01_CCD_CreateCaseEndpoint_Probate_ApplyGrantApplication_eventtoken")
+        .get(CreateCaseTokenUrl)
+        .header("ServiceAuthorization", token)
+        .header("Authorization", userToken)
+        .header("Content-Type","application/json")
+        .check(status.is(200),jsonPath("$.token").saveAs("eventToken"))
+    ).exec(
+      //http("create case data")
+      http("TX02_CCD_CreateCaseEndpoint_Probate_ApplyGrantApplication_createcasedata")
+        .post(CreateCaseUrl)
+        .body(CreateProbateGrantCaseDataEventBodyMain).asJSON
+        .header("ServiceAuthorization", token)
+        .header("Authorization", userToken)
+        .header("Content-Type","application/json")
+        .check(status is 201)
+    )
+
+      .pause(MinThinkTime seconds, MaxThinkTime seconds)
+  }
+
+
+  val waitForNextIteration = pace(MinWaitForNextIteration seconds, MaxWaitForNextIteration seconds)
+}
+
--- src/test/scala/uk/gov/hmcts/ccd/simulation/CCDPTSimulation.scala
+++ src/test/scala/uk/gov/hmcts/ccd/simulation/CCDPTSimulation.scala
@@ -14,9 +14,88 @@ class CCDPTSimulation extends CCDSimulation {
   val baseHttpUrl: String = config.getString("caseDataUrl")
 
 
+  /*
+   val scenarios = List(
+      GetCaseData.scenarios.inject(
+        atOnceUsers(1)),
+      PostCaseData.createCaseData.inject(
+        atOnceUsers(1)),
+      SearchCases.searchCases.inject(
+        atOnceUsers(10)),
+      PostEvent.saveEventData.inject(
+        atOnceUsers(1)),
+      GetUserProfile.scenarios.inject(
+        atOnceUsers(1))
+    )
+   */
+
+
+  /*
+  User Concurrency Stress Test
+   minthinktime = 2
+  maxthinktime = 3
+  minWaitForNextIteration = 4
+  maxWaitForNextIteration = 5
+  totalDuration = 1000
+
+
+
+  val scenarios = List(
+    GetCaseData.scenarios.inject(splitUsers(1000) into(rampUsers(10) over(5 minutes)) separatedBy(5 minutes)),
+    PostCaseData.createCaseData.inject(splitUsers(1000) into(rampUsers(10) over(5 minutes)) separatedBy(5 minutes)),
+    SearchCases.searchCases.inject(splitUsers(1000) into(rampUsers(10) over(5 minutes)) separatedBy(5 minutes)),
+    PostEvent.saveEventData.inject(splitUsers(1000) into(rampUsers(10) over(5 minutes)) separatedBy(5 minutes)),
+    GetUserProfile.scenarios.inject(splitUsers(1000) into(rampUsers(10) over(5 minutes)) separatedBy(5 minutes))
+  )
+*/
+
+
+
+  /* Single User Test
+   minthinktime = 2
+    maxthinktime = 3
+    minWaitForNextIteration = 4
+    maxWaitForNextIteration = 5
+    totalDuration = 2
+
+ES Load Testing
+ minthinktime = 7
+  maxthinktime = 9
+  minWaitForNextIteration = 10
+  maxWaitForNextIteration = 12
+  totalDuration = 60
+
+  ES Case count in CCD - 200685
+
+*/
+
   val scenarios = List(
     //GetCaseDataV2.GetCaseDataV2Scenarios.inject(rampUsers(500) over(15 minutes))
-   CreateCMCCaseData.CreateCMCCaseDataSCN.inject(rampUsers(1) over(1 minutes))
+    CreateProbateGrantCaseData.CreateProbateGrantCaseDataSCN.inject(rampUsers(1) over(1 minutes))
+   //CreateCMCCaseData.CreateCMCCaseDataSCN.inject(rampUsers(100) over(5 minutes)),
+   //CreateSSCSCaseData.CreateSSCSCaseDataSCN.inject(rampUsers(1) over(1 minutes)),
+   //CreateDIVCaseData.CreateDIVCaseDataSCN.inject(rampUsers(1) over(1 minutes)),
+    //ProdDIVORCESearchD8caseReference.ProdDIVORCESearchD8caseReferenceSCN.inject(rampUsers(300) over(5 minutes)),
+    //ProdDIVORCESearchD8caseReferencePagination.ProdDIVORCESearchD8caseReferencePaginationSCN.inject(rampUsers(300) over(5 minutes)),
+    //ProdSSCSSearchPagination.ProdSSCSSearchPaginationSCN.inject(rampUsers(300) over(5 minutes)),
+   //ProdSSCSSearchWorkBasketIssue.ProdSSCSSearchWorkBasketIssueSCN.inject(rampUsers(300) over(5 minutes))
+    //  PostCaseData.createCaseData.inject(rampUsers(1) over(1 minutes)),
+   //   CrossCaseTypeAliasfuzzyYesOrNo.CrossCaseTypeAliasfuzzyYesOrNoSCN.inject(rampUsers(1) over(1 minutes)),
+    //  CrossCaseTypeAliasMatchYesOrNo.CrossCaseTypeAliasMatchYesOrNoSCN.inject(rampUsers(1) over(1 minutes)),
+    //  CrossCaseTypeAliasMatchOnTextArea.CrossCaseTypeAliasMatchOnTextAreaSCN.inject(rampUsers(1) over(1 minutes)),
+    //  CrossCaseTypeAliasMatchOnText.CrossCaseTypeAliasMatchOnTextSCN.inject(rampUsers(1) over(1 minutes)),
+   // CrossCaseTypeAliasFuzzySearchOnText.CrossCaseTypeAliasFuzzySearchOnTextSCN.inject(rampUsers(1) over(1 minutes))
+    //  ESMatchAllCases.ESMatchAll_Return50Cases.inject(rampUsers(1) over(1 minutes)),
+    //  ESExactMatchYesOrNo.ESExactMatchYesOrNoSCN.inject(rampUsers(1) over(1 minutes)),
+     // ESSeachONTextArea.ESSeachONTextAreaSCN.inject(rampUsers(1) over(1 minutes)),
+     // ESSeachONReferenceMetaData.ESSeachONReferenceMetaDataSCN.inject(rampUsers(1) over(1 minutes))
+   // ESStdTest.ESStdTEST.inject(rampUsers(1) over(5 minutes))
+   // GetCaseData.scenarios.inject(rampUsers(1) over(1 minutes)),
+  //  PostCaseData.createCaseData.inject(rampUsers(1) over(1 minutes)),
+  //  SearchCases.searchCases.inject(rampUsers(1) over(15 minutes)),
+  //  SearchInputDetails.getSearchInputDetails.inject(rampUsers(1) over(1 minutes)),
+   // WorkBasketInputDetails.getWorkbasketInputDetails.inject(rampUsers(1) over(15 minutes)),
+   // GetPaginationMetaData.getPaginationMetaData.inject(rampUsers(1) over(1 minutes)),
   )
 
 
